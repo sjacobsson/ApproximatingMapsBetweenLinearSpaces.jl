dd1c0fb7319933fbc9e2d7030c218fe72020dc68
Simon Jacobsson <simonj@beltrami.dyn.cs.kuleuven.be>
2023-06-14 11:55:05 +0200

    added error bounds for dominant_singular_value()

--- QOL.jl
+++ QOL.jl
@@ -8,32 +8,41 @@ function pa(f, a...; pos=1)#={{{=#
     return (b...) -> f([b...][1:(pos - 1)]..., a..., [b...][(pos + length([a...]) - 1):end]...)
 end#=}}}=#
 
-# """ Approximate derivative of f at x """
-# function finite_difference(#={{{=#
-#     f::Function, # :: ℝ -> some vector space
-#     x::Float64,
-#     h::Float64;
-#     order=1::Int64
-#     )
+""" Approximate derivative of f at x """
+function finite_difference(#={{{=#
+    f::Function, # :: ℝ -> some vector space
+    x::Float64,
+    h::Float64;
+    order=1::Int64
+    )
 
-#     # https://en.wikipedia.org/wiki/Finite_difference_coefficient
-#     if order == 1
-#         return (
-#             (1 / 12) *  f(x - 2 * h) +
-#             (-2 / 3) *  f(x - 1 * h) +
-#             (2 / 3) *   f(x + 1 * h) +
-#             (-1 / 12) * f(x + 2 * h)
-#             ) / h
-#     elseif order == 2
-#     return (
-#         (-1 / 12) *  f(x - 2 * h) +
-#         (4 / 3) *  f(x - 1 * h) +
-#         (-5 / 2) *   f(x) +
-#         (4 / 3) *  f(x + 1 * h) +
-#         (-1 / 12) * f(x + 2 * h)
-#         ) / h
-#     end
-# end#=}}}=#
+    # https://en.wikipedia.org/wiki/Finite_difference_coefficient
+    if order == 1
+        return (
+            (1 / 12) *  f(x - 2 * h) +
+            (-2 / 3) *  f(x - 1 * h) +
+            (2 / 3) *   f(x + 1 * h) +
+            (-1 / 12) * f(x + 2 * h)
+            ) / h
+    elseif order == 2
+    return (
+        (-1 / 12) * f(x - 2 * h) +
+        (4 / 3) *   f(x - 1 * h) +
+        (-5 / 2) *  f(x) +
+        (4 / 3) *   f(x + 1 * h) +
+        (-1 / 12) * f(x + 2 * h)
+        ) / h^2
+    elseif order == 3
+    return (
+        (1 / 8) *   f(x - 3 * h) +
+        (-1) *      f(x - 2 * h) +
+        (13 / 8) *  f(x - 1 * h) +
+        (-13 / 8) * f(x + 1 * h) +
+        (1) *       f(x + 2 * h) +
+        (-1 / 8) *  f(x + 3 * h)
+        ) / h^3
+    end
+end#=}}}=#
 
 import Base.+#={{{=#
 import Base.-
--- error plots/Errors.jl
+++ error plots/Errors.jl
@@ -1,6 +1,7 @@
 # Compute errors from approximating different functions and compare to the error
 # bound predicted by thm TODO.
 include("../Approximations.jl")
+include("../QOL.jl")
 using Plots, LinearAlgebra, Random, Combinatorics, Transducers
 
 Lambda(N) = (2 / pi) * log(N + 1) + 1
@@ -99,14 +100,59 @@ function gaussian(;#={{{=#
     display(p)
 end#=}}}=#
 
+# C^1
+function gaussian_modified(;#={{{=#
+    m=4,
+    Ns=2:2:26,
+    verbose=false,
+    savefigure=false,
+    kwargs...
+    )
+    function g(x::Vector{Float64})::Float64
+        return exp(-sum([sign(xi) * xi^2 for xi in x]))
+    end
+    
+    V2 = 4 * exp(m)
+    
+    es = [NaN for _ in Ns]
+    bs = [NaN for _ in Ns]
+    for (i, N) = enumerate(Ns)
+        if verbose; println(i, "/", length(Ns)); end
+
+        bs[i] = 4 * V2 * (Lambda(N)^m - 1) / (pi * 2 * big(N - 2)^2 * (Lambda(N) - 1))
+    
+        ghat = approximate_scalar(m, g; res=N, kwargs...)
+        # TODO: calculate max betterly
+        es[i] = maximum([
+            abs((g - ghat)(2 * rand(m) .- 1.0))
+            for _ in 1:1000])
+        if verbose; println("error ", es[i]); end
+    end
+    
+    p = plot(Ns, bs;
+        label="error bound",
+        xlabel="N",
+        xticks=Ns,
+        yaxis=:log,
+        # ylims=(1e-16, 2 * maximum([es..., bs...])),
+        # yticks=([1e0, 1e-5, 1e-10, 1e-15]),
+        )
+    scatter!(Ns, es;
+        label="measured error")
+    if savefigure; savefig("gaussian_modified.pdf"); end
+    display(p)
+end#=}}}=#
+
 # Smooth
+# Use tol=1e-15 to unlock full precision
 function dominant_singular_value(;#={{{=#
     m=4,
-    n1=20,
-    n2=30,
+    n1=40,
+    n2=60,
     Ns=2:1:12,
     verbose=false,
     savefigure=false,
+    figkwargs=(windowsize=(240, 160), guidefontsize=5, xtickfontsize=5, ytickfontsize=5, legendfontsize=5),
     kwargs...
     )
 
@@ -121,11 +167,71 @@ function dominant_singular_value(;#={{{=#
             )
         return S[1]
     end
+
+    # nth derivative of the svd along axis l #={{{=#
+    # https://www.jstor.org/stable/2695472
+    g1(l) = big(norm(finite_difference(t -> g([i == l ? t : 0 for i in 1:m]), 0.0, 1e-5, order=1)))
+    ginv1(l) = 1 / g1(l)
+    g2(l) = big(norm(finite_difference(t -> g([i == l ? t : 0 for i in 1:m]), 0.0, 1e-4, order=2)))
+    ginv2(l) = -g2(l) / g1(l)^3
+    g3(l) = big(norm(finite_difference(t -> g([i == l ? t : 0 for i in 1:m]), 0.0, 1e-3, order=3)))
+    ginv3(l) = 1 / g1(l)^5 * ( 3* g2(l)^2 - g1(l) * g3(l) )
+
+    V1(l) = 2 * abs(g1(l))
+    V2(l) = 2 * abs(g2(l))
+    V3(l) = 2 * abs(g3(l))
+    V4(l) = 2 * abs(1 / ginv1(l)^7 * (
+        -15 * ginv2(l)^3 +
+        10 * ginv1(l) * ginv2(l) * ginv3(l)
+        ))
+    V5(l) = 2 * abs(1 / ginv1(l)^9 * (
+        105 * ginv2(l)^4 -
+        105 * ginv1(l) * ginv2(l)^2 * ginv3(l) +
+        10 * ginv1(l)^2 * ginv3(l)^2
+        ))
+    V6(l) = 2 * abs(1 / ginv1(l)^11 * (
+        -945 * ginv2(l)^5 +
+        1260 * ginv1(l) * ginv2(l)^3 * ginv3(l) -
+        280 * ginv1(l)^2 * ginv2(l) * ginv3(l)^2
+        ))
+    V7(l) = 2 * abs(1 / ginv1(l)^13 * (
+        10395 * ginv2(l)^6 -
+        17325 * ginv1(l) * ginv2(l)^4 * ginv3(l) +
+        6300 * ginv1(l)^2 * ginv2(l)^2 * g3(l)^2 -
+        280 * g1(l)^3 * g3(l)^3
+        ))
+    V8(l) = 2 * abs(1 / g1(l)^15 * (
+        -135135 * g2(l)^7 +
+        270270 * g1(l) * g2(l)^5 * g3(l) -
+        138600 * g1(l)^2 * g2(l)^3 * g3(l)^2 +
+        15400 * g1(l)^3 * g2(l) * g3(l)^3
+        ))
+    V9(l) = 2 * abs( 1 / g1(l)^17 * (
+        2027025 * g2(l)^8 -
+        4729725 * g1(l) * g2(l)^6 * g3(l) +
+        3153150 * g1(l)^2 * g2(l)^4 * g3(l)^2 -
+        600600 * g1(l)^3 * g2(l)^2 * g3(l)^3 + 
+        15400 * g1(l)^4 * g3(l)^4
+        ))
+    V10(l) = 2 * abs(1 / g1(l)^19 * (
+        -34459425 * g2(l)^9 +
+        91891800 * g1(l) * g2(l)^7 * g3(l) -
+        75675600 * g1(l)^2 * g2(l)^5 * g3(l)^2 +
+        21021000 * g1(l)^3 * g2(l)^3 * g3(l)^3 -
+        1401400 * g1(l)^4 * g2(l) * g3(l)^4
+        ))#=}}}=#
+
+    V(nu) = maximum([[V1, V2, V3, V3, V4, V5, V6, V7, V8, V9, V10, repeat([t -> NaN], 100)...][nu](l) for l in 1:m])
     
+    global ghat
     es = [NaN for _ in Ns]
-    global ghat # TODO: try local?
+    bs = [NaN for _ in Ns]
     for (i, N) = enumerate(Ns)
         if verbose; println(i, "/", length(Ns)); end
+
+        bs[i] = minimum([
+            4 * V(nu) * (Lambda(N)^m - 1) / (pi * nu * big(N - nu)^nu * (Lambda(N) - 1))
+            for nu in 1:(N - 1)])
 
         ghat = approximate_scalar(m, g; res=N, kwargs...)
         # TODO: calculate max betterly
@@ -133,19 +239,25 @@ function dominant_singular_value(;#={{{=#
             abs((g - ghat)(2 * rand(m) .- 1.0))
             for _ in 1:1000])
 
-        if verbose; println("error ", es[i]); end
+        if verbose
+            println("error ", es[i])
+            x = ones(m) - 2.0 * rand(m)
+            print("evaluating g     ")
+            @time(g(x))
+            print("evaluating ghat  ")
+            @time(ghat(x))
+            println()
+        end
     end
 
-    # x = rand(m)
-    # @time g(x)
-    # @time ghat(x)
-    
-    p = plot(;
+    p = plot(Ns, bs;
+        label="error bound",
         xlabel="N",
         xticks=Ns,
         yaxis=:log,
-        ylims=(1e-16, 2 * maximum(es)),
+        ylims=(1e-16, 2 * maximum([es..., bs...])),
         yticks=([1e0, 1e-5, 1e-10, 1e-15]),
+        figkwargs...
         )
     scatter!(Ns, es;
         label="measured error",
@@ -214,54 +326,54 @@ function ackley(;#={{{=#
     display(p)
 end#=}}}=#
 
-# C^1 so converges linearly
-function alpine(;#={{{=#
-    m=3,
-    Ns=20:20:200,
-    verbose=false,
-    savefigure=false,
-    kwargs...
-    )
+# TODO: This is not C^1...
+# function alpine(;#={{{=#
+#     m=3,
+#     Ns=20:20:200,
+#     verbose=false,
+#     savefigure=false,
+#     kwargs...
+#     )
 
-    V1 = 672.614 # integral of abs( d^2/dx^2 abs(x * 10 * sin(x * 10) + 0.1 * x * 10)) from x=-1 to x=1
+#     V1 = 672.614 # integral of abs( d^2/dx^2 abs(x * 10 * sin(x * 10) + 0.1 * x * 10)) from x=-1 to x=1
 
-    function g(xs_)
-        d = length(xs_)
-        xs = xs_ * 10.0
+#     function g(xs_)
+#         d = length(xs_)
+#         xs = xs_ * 10.0
 
-        return sum([abs(x * sin(x) + 0.1 * x) for x in xs])
-    end
+#         return sum([abs(x * sin(x) + 0.1 * x) for x in xs])
+#     end
 
-    es = [NaN for _ in Ns]
-    bs = [NaN for _ in Ns]
-    for (i, N) = enumerate(Ns)
-        if verbose; println(i, "/", length(Ns)); end
+#     es = [NaN for _ in Ns]
+#     bs = [NaN for _ in Ns]
+#     for (i, N) = enumerate(Ns)
+#         if verbose; println(i, "/", length(Ns)); end
 
-        ghat = approximate_scalar(m, g; res=N, kwargs...)
+#         ghat = approximate_scalar(m, g; res=N, kwargs...)
 
-        bs[i] = 4 * V1 * (Lambda(N)^m - 1) / (pi * 1 * (N - 1)^1 * (Lambda(N) - 1))
-        # TODO: calculate max betterly
-        es[i] = maximum([
-            abs((g - ghat)(2 * rand(m) .- 1.0))
-            for _ in 1:1000])
-        if verbose; println("error ", es[i]); end
-    end
+#         bs[i] = 4 * V1 * (Lambda(N)^m - 1) / (pi * 1 * (N - 1)^1 * (Lambda(N) - 1))
+#         # TODO: calculate max betterly
+#         es[i] = maximum([
+#             abs((g - ghat)(2 * rand(m) .- 1.0))
+#             for _ in 1:1000])
+#         if verbose; println("error ", es[i]); end
+#     end
     
-    p = plot(Ns, bs;
-        label="error bound",
-        xlabel="N",
-        xticks=Ns,
-        yaxis=:log,
-        # ylims=(1e-16, 2 * maximum([es...])),
-        # yticks=([1e0, 1e-5, 1e-10, 1e-15]),
-        )
-    scatter!(Ns, es;
-        label="measured error",
-        color=2
-        )
-    if savefigure; savefig("alpine.pdf"); end
-    display(p)
-end#=}}}=#
+#     p = plot(Ns, bs;
+#         label="error bound",
+#         xlabel="N",
+#         xticks=Ns,
+#         yaxis=:log,
+#         # ylims=(1e-16, 2 * maximum([es...])),
+#         # yticks=([1e0, 1e-5, 1e-10, 1e-15]),
+#         )
+#     scatter!(Ns, es;
+#         label="measured error",
+#         color=2
+#         )
+#     if savefigure; savefig("alpine.pdf"); end
+#     display(p)
+# end#=}}}=#
 
 # Smooth, so converges exponentially
 function rastrigin(;#={{{=#
@@ -542,7 +654,7 @@ function michalewicz(;#={{{=#
     display(p)
 end#=}}}=#
 
-# TODO:  test_approximate_vector
+# TODO: test_approximate_vector
 function smooth_vector_field(;#={{{=#
     m=4,
     n=4,
